7681036ae468a707c176463555a2f7421fe5ee5d
团子 <876924196@qq.com>
2026-08-17 13:21:57 +0800

    test: add bug3 verification

---
+++ internal/classroom/assignment_close_private_test.go
@@ -0,0 +1,3 @@
+﻿package classroom
+import("testing";"time")
+func TestClosingOneAssignmentDoesNotCloseAnother(t *testing.T){s:=NewStore();c,_:=s.CreateCourse("Math","teacher");a1,_:=s.CreateAssignment(c.ID,"one",time.Now().Add(time.Hour));a2,_:=s.CreateAssignment(c.ID,"two",time.Now().Add(time.Hour));if err:=s.CloseAssignment(c.ID,a1.ID);err!=nil{t.Fatal(err)};s.mu.RLock();closed2:=s.assignments[a2.ID].Closed;s.mu.RUnlock();if closed2{t.Fatal("unrelated assignment was closed")}}
